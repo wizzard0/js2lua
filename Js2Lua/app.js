c0aa838eff7ed03a15bf7fcb3d7393e8d2c5894f
wizzard <wizzard@localhost>
2015-01-12 08:25:34 +0200

    vars

--- Js2Lua/app.js
+++ Js2Lua/app.js
@@ -2,13 +2,18 @@ var esprima = require("esprima");
 var util = require("util");
 var print = console.log;
 var tc = function t() {
-    print("foobar", 2 + 2);
+    var s = 2 + 2;
+    var adder = function (a, b) {
+        return a + b;
+    };
+    print(s);
+    print(adder(s, s));
+    print("foobar");
 };
 var source = tc.toString();
 console.log('js2lua');
 console.log(source);
 var ast = esprima.parse(source);
-console.log(util.inspect(ast, false, 999, true));
 var luasrc = "";
 var emit = function (code) {
     luasrc += code;
@@ -23,20 +28,38 @@ function EmitProgram(ast, emit) {
         switch (stmt.type) {
             case "ExpressionStatement":
                 EmitExpression(stmt.expression, emit);
+                emit(";\r\n");
+                break;
+            case "VariableDeclaration":
+                EmitVariableDeclaration(stmt, emit);
                 break;
             default:
                 emit("--[[");
                 emit(stmt.type);
                 emit("]]");
+                console.log(util.inspect(stmt, false, 999, true));
                 break;
         }
     }
     emit("\r\n-- END\r\n");
 }
+function EmitVariableDeclaration(ex, emit) {
+    for (var i = 0; i < ex.declarations.length; i++) {
+        var vd = ex.declarations[i];
+        emit("local ");
+        EmitExpression(vd.id, emit); // identifier
+        emit(" = ");
+        EmitExpression(vd.init, emit);
+        emit(";\r\n");
+    }
+}
 function EmitExpression(ex, emit) {
     switch (ex.type) {
         case "CallExpression":
             EmitCall(ex, emit);
+            break;
+        case "BinaryExpression":
+            EmitBinary(ex, emit);
             break;
         case "Identifier":
             emit(ex.name);
@@ -48,8 +71,16 @@ function EmitExpression(ex, emit) {
             emit("--[[");
             emit(ex.type);
             emit("]]");
+            console.log(util.inspect(ex, false, 999, true));
             break;
     }
+}
+function EmitBinary(ast, emit) {
+    emit("(");
+    EmitExpression(ast.left, emit);
+    emit(ast.operator);
+    EmitExpression(ast.right, emit);
+    emit(")");
 }
 function EmitCall(ast, emit) {
     EmitExpression(ast.callee, emit);
--- Js2Lua/app.ts
+++ Js2Lua/app.ts
@@ -4,14 +4,19 @@ import util = require("util");
 var print = console.log;
 
 var tc = function t() {
-    print("foobar", 2 + 2);
+    var s = 2 + 2;
+    var adder = function (a, b) {
+        return a + b;
+    }
+    print(s);
+    print(adder(s, s));
+    print("foobar");
 };
 var source = tc.toString();
 console.log('js2lua');
 console.log(source);
 
 var ast = esprima.parse(source);
-console.log(util.inspect(ast, false, 999, true));
 
 var luasrc = "";
 var emit = function (code) {
@@ -29,15 +34,31 @@ function EmitProgram(ast: esprima.Syntax.Program, emit: (s: string) => void) {
         switch (stmt.type) {
             case "ExpressionStatement":
                 EmitExpression((<esprima.Syntax.ExpressionStatement>stmt).expression, emit);
+                emit(";\r\n");
+                break;
+            case "VariableDeclaration":
+                EmitVariableDeclaration((<esprima.Syntax.VariableDeclaration>stmt), emit);
                 break;
             default:
                 emit("--[[");
                 emit(stmt.type);
                 emit("]]");
+                console.log(util.inspect(stmt, false, 999, true));
                 break;
         }
     }
     emit("\r\n-- END\r\n");
+}
+
+function EmitVariableDeclaration(ex: esprima.Syntax.VariableDeclaration, emit: (s: string) => void) {
+    for (var i = 0; i < ex.declarations.length; i++) {
+        var vd = ex.declarations[i];
+        emit("local ");
+        EmitExpression(vd.id, emit); // identifier
+        emit(" = ");
+        EmitExpression(vd.init, emit);
+        emit(";\r\n");
+    }
 }
 
 function EmitExpression(ex: esprima.Syntax.Expression, emit: (s: string) => void) {
@@ -45,6 +66,9 @@ function EmitExpression(ex: esprima.Syntax.Expression, emit: (s: string) => void
     switch (ex.type) {
         case "CallExpression":
             EmitCall(<esprima.Syntax.CallExpression>ex, emit);
+            break;
+        case "BinaryExpression":
+            EmitBinary(<esprima.Syntax.BinaryExpression>ex, emit);
             break;
         case "Identifier":
             emit((<esprima.Syntax.Identifier>ex).name);
@@ -56,8 +80,17 @@ function EmitExpression(ex: esprima.Syntax.Expression, emit: (s: string) => void
             emit("--[[");
             emit(ex.type);
             emit("]]");
+            console.log(util.inspect(ex, false, 999, true));
             break;
     }
+}
+
+function EmitBinary(ast: esprima.Syntax.BinaryExpression, emit: (s: string) => void) {
+    emit("(");
+    EmitExpression(ast.left, emit);
+    emit(ast.operator);
+    EmitExpression(ast.right, emit);
+    emit(")");
 }
 
 function EmitCall(ast: esprima.Syntax.CallExpression, emit: (s: string) => void) {
