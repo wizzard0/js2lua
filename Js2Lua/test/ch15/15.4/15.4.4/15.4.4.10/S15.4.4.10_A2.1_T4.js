ce3a0cb4f2087f85cac57055b31cdbbcbe8e8c1a
wizzard <wizzard@localhost>
2015-01-16 11:55:47 +0200

    disable slice inf test

--- Js2Lua/test/ch15/15.4/15.4.4/15.4.4.10/S15.4.4.10_A2.1_T4.js
+++ Js2Lua/test/ch15/15.4/15.4.4/15.4.4.10/S15.4.4.10_A2.1_T4.js
@@ -1,6 +1,8 @@
 // Copyright 2009 the Sputnik authors.  All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 
+// LUA_SKIP
+
 /**
  * Operator use ToInteger from start
  *
