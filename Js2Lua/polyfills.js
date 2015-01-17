945c4752f54a7fddd713ecdc629bc7494606ddc7
wizzard <wizzard@localhost>
2015-01-17 02:09:28 +0200

    reenable keys

--- Js2Lua/polyfills.js
+++ Js2Lua/polyfills.js
@@ -60,46 +60,46 @@ Object.defineProperties = function(obj, properties) {
 }
 
 // From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
-// if (!Object.keys) {
-//   Object.keys = (function() {
-//     'use strict';
-//     var hasOwnProperty = Object.prototype.hasOwnProperty,
-//         hasDontEnumBug = !({ toString: null }).propertyIsEnumerable('toString'),
-//         dontEnums = [
-//           'toString',
-//           'toLocaleString',
-//           'valueOf',
-//           'hasOwnProperty',
-//           'isPrototypeOf',
-//           'propertyIsEnumerable',
-//           'constructor'
-//         ],
-//         dontEnumsLength = dontEnums.length;
+if (!Object.keys) {
+  Object.keys = (function() {
+    'use strict';
+    var hasOwnProperty = Object.prototype.hasOwnProperty,
+        hasDontEnumBug = !({ toString: null }).propertyIsEnumerable('toString'),
+        dontEnums = [
+          'toString',
+          'toLocaleString',
+          'valueOf',
+          'hasOwnProperty',
+          'isPrototypeOf',
+          'propertyIsEnumerable',
+          'constructor'
+        ],
+        dontEnumsLength = dontEnums.length;
 
-//     return function(obj) {
-//       if (typeof obj !== 'object' && (typeof obj !== 'function' || obj === null)) {
-//         throw new TypeError('Object.keys called on non-object');
-//       }
+    return function(obj) {
+      if (typeof obj !== 'object' && (typeof obj !== 'function' || obj === null)) {
+        throw new TypeError('Object.keys called on non-object');
+      }
 
-//       var result = [], prop, i;
+      var result = [], prop, i;
 
-//       for (prop in obj) {
-//         if (hasOwnProperty.call(obj, prop)) {
-//           result.push(prop);
-//         }
-//       }
+      for (prop in obj) {
+        if (hasOwnProperty.call(obj, prop)) {
+          result.push(prop);
+        }
+      }
 
-//       if (hasDontEnumBug) {
-//         for (i = 0; i < dontEnumsLength; i++) {
-//           if (hasOwnProperty.call(obj, dontEnums[i])) {
-//             result.push(dontEnums[i]);
-//           }
-//         }
-//       }
-//       return result;
-//     };
-//   }());
-// }
+      if (hasDontEnumBug) {
+        for (i = 0; i < dontEnumsLength; i++) {
+          if (hasOwnProperty.call(obj, dontEnums[i])) {
+            result.push(dontEnums[i]);
+          }
+        }
+      }
+      return result;
+    };
+  }());
+}
 
 // Production steps of ECMA-262, Edition 5, 15.4.4.19
 // Reference: http://es5.github.io/#x15.4.4.19
