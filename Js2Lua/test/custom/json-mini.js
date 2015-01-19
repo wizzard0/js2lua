f069327526798d86cea69be2f03c01bd38b830a0
wizzard <wizzard@localhost>
2015-01-19 08:01:48 +0200

    jm test

--- Js2Lua/test/custom/json-mini.js
+++ Js2Lua/test/custom/json-mini.js
@@ -1,4 +1,5 @@
-var x = {a:1,b:[1,2,3]};
+function test(x){
+try{
 
 var y = JSON.stringify(x);
 console.log(y);
@@ -7,3 +8,11 @@ var z = JSON.parse(y);
 
 var w = JSON.stringify(z);
 console.log(w);
+}catch(e){
+	console.log("ERROR:"+e);
+}
+}
+
+test({});
+test({a:1});
+test({a:1,b:[1,2,3]});
